cb93f8303defa8ca7bd94bcbfecd15ebbddba490
Paul Rosenfeld <dramninjas@gmail.com>
2010-08-03 20:46:17 -0400

    Added some documentation to vmutils along with a script for using SPEC

--- vmutils.sh
+++ vmutils.sh
@@ -1,66 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+# Source this file (it won't do anything if you just run it) and then use the
+# commands: vmsetup, vmclone, vmmount, vmumount, vmchroot
+#
+# This file contains a set of commands that make it easier to deal with marss disk images 
+# I tried to briefly describe what the functions do, but if you need help
+# you can send an email to dramninjas (at) gmail (dot) com and I can try
+# to explain how to use these.
+#
+# The gist is that I have 3 raw disk images hda.raw (/root in the VM), hdb.raw (some
+# benchmarks like SPEC06 mounted at /root/mnt in the VM), and hdc.raw (containing
+# some autorun script at /root/sim/simulate.sh which will be auto run when the
+# VM starts). These disk images are copied and the appropriate simulate.sh file is
+# loaded into each one when vmsetup is called. vmrun then launches the virtual machine
+# and the simulation within the VM begins automatically, allowing one to start up many
+# instances at once -- only limited by processors and memory.
+#
+# The root image needs a bit of work to make this happen (such as autologin as
+# root with mingetty) and modifications to /root/.bashrc, but the vmchroot
+# command makes it easy to add these without having to actually run the image in QEMU
+
+
+#adjust these paths to taste
 QEMU_IMG_BIN="$HOME/marss.dramsim/qemu/qemu-img"
 IMAGE_DIR="$HOME/disks"
 MOUNT_POINT="$IMAGE_DIR/mnt"
+# referred to from here on out as the "base image" 
 IMAGE_NAME="parsec.base.raw"
 IMAGE_PATH="$IMAGE_DIR/$IMAGE_NAME"
+
 MARSS_DIR=$HOME/marss.dramsim
 
-# so I'm not sure why something like dirname $HOME returns /home when I'd
-# expect it to return /home/USER, but I need to make a function that is sane --
-# i.e. if the argument is a path to a file, return the directory the file is in,
-# but if the directory is already a directory, just return the absolute path to it
-function __absdirname() {
-	if [ -d "$1" ] ; then
-		echo $(__abspath "$1")
-	else
-		echo $(__abspath $(dirname "$1"))
-	fi
-}
 
-function __abspath() {
-	if [ -d "$1" ] ; then 
-		local DIR=$1
-	else 
-		local DIR=$(dirname "$1")
-	fi
-	__vmpushd $DIR
-	echo $PWD
-	__vmpopd
-}
+######################## The actual "public" functions ######################
 
-function __mount_image () {
-	local PARTITION_NUM=1
-	
-	if [ -e "$1" ] ; then # a real filename given 
-		local FILENAME=$( basename "$1" )
-		local FILE_DIR=$( __absdirname "$1" )
-	else
-		local FILENAME="$IMAGE_FILE"
-		local FILE_DIR="$IMAGE_DIR"
-	fi
-
-	local PARTITION_TYPE="ext2"
-	echo "Mounting $PARTITION_TYPE partition: image=$FILE_DIR/$FILENAME to $MOUNT_POINT ... "
-
-	local OFFSET=`sfdisk -l -uS "$FILE_DIR/$FILENAME" 2> /dev/null | grep "$FILENAME$PARTITION_NUM" | awk '{print $2}'`
-	local SECTOR_OFFSET=$((OFFSET*512))
-	sudo mount -t $PARTITION_TYPE -o loop,offset=$SECTOR_OFFSET "$FILE_DIR/$FILENAME" "$MOUNT_POINT"
-}
-
-function __umount_image () {
-	echo -n "unmounting image from $MOUNT_POINT ... "
-	__vmpushd $HOME 
-	sudo umount "$MOUNT_POINT";
-	__vmpopd 
-	sync
-	echo "OK"
-}
-
-#for symmetry's sake
-function vmumount() {
-	__umount_image
-}
 
 function vmmount () {
 	__mount_image $1
@@ -71,14 +44,14 @@ function vmmount () {
 	__vmpopd 
 }
 
-# get rid of the annoying output of these commands
-function __vmpushd() {
-	pushd "$1" > /dev/null
-}
-function __vmpopd() {
-	popd > /dev/null
+#for symmetry's sake
+function vmumount() {
+	__umount_image
 }
 
+# vmclone -- Takes a raw disk image and creates a qcow2 copy of it
+# run  'vmclone output.qcow2'  to make a copy of the "base image" 
+# or run 'vmclone someimage.raw output.qcow2' if you want to make a clone of some other image
 function vmclone() {
 #TODO: maybe use getopt to add a -force flag to override the output file check
 	if [ "$#" == "2" ] ; then 
@@ -115,6 +88,11 @@ function vmclone() {
 	$QEMU_IMG_BIN convert -f raw "$INPUT_FILENAME" -O qcow2 "$OUTPUT_FILENAME"
 }
 
+# vmrun x -- launch VM #x with the proper parameters for that number
+# use 'vmsetup x' before using the vmrun command. Note: the VM will launch 
+# in a detached screen. Please read up on how to use screen if you are 
+# not familiar with it, or just remove the call to screen to run as a foreground
+# task 
 
 function vmrun() {
 	if [ -z "$1" ] ; then
@@ -122,20 +100,25 @@ function vmrun() {
 		return
 	fi 
 
+	__vmpushd "$MARSS_DIR"
 	# sed sure is hard to read ... this just grabs the argument of SIM_DESC= from the simulate file
 	local SIM_DESCRIPTION=`cat simulate$1.sh | sed -n -e 's/^#SIM_DESC="\([^"]*\)"/\1/p '`
 	if [ -z "$SIM_DESCRIPTION" ] ; then 
-		echo "No sim description -- did you use vmsetup? "
+		echo "No sim description in simulate$1.sh -- did you use vmsetup? (not launching sim)"
+		__vmpopd
 		return
-	else 
 	fi 
 
-	"Launching simulation #$1: $SIM_DESCRIPTION"
-	__vmpushd "$MARSS_DIR"
+	echo "Launching simulation #$1: $SIM_DESCRIPTION"
 	local CMD_TO_RUN="SIM_DESC=\"$SIM_DESCRIPTION\" gdb -x gdbrun -args qemu/qemu-system-x86_64 -m 2GB -simconfig \"simconfig$1.cfg\" -hda \"hda$1.qcow2\" -hdb \"hdb$1.qcow2\" -hdc \"hdc$1.raw\" -curses"
-	screen -S "sim$1" $CMD_TO_RUN
+	screen -d -m -S "sim$1" bash -c "$CMD_TO_RUN"
 	__vmpopd
 }
+
+# vmsetup x desc -- setup vm x with a sim description 
+# This command creates the necessary files, copies a simulate.sh file into the hdc disk image which can be auto launched if the VM is setup the right way.
+# This function does the heavy lifting of this script -- if it sounds interesting, send dramninjas (at) gmail (dot) com an email and I can explain how
+# the disk images are supposed to be set up for this to work right
 
 function vmsetup() {
 	if [ $# -lt 1 ] ; then 
@@ -191,6 +174,85 @@ done
 __vmpopd
 }
 
+# vmmultirun x y -- setup and run VMs numbered x to y 
+function vmmultirun () {
+	if [ $# -lt 2 ] ; then
+		echo "usage: $0 start end"
+		return
+	fi
+	for i in `seq $1 $2` 
+	do
+		vmsetup $i
+		vmrun $i
+	done
+}
+
+# vmchroot -- mount the base image and chroot into it
+# this is useful for mounting the root image and using it
+# without having to actually start up QEMU. Things like 
+# installing packages or compiling 
+function vmchroot() {
+	__mount_image $IMAGE_PATH; 
+	__mount_proc_sys_dev;
+	sudo chroot $MOUNT_POINT
+	__unmount_proc_sys_dev;
+	vmumount;
+}
+
+
+################## Utility functions #########################
+
+# so I'm not sure why something like dirname $HOME returns /home when I'd
+# expect it to return /home/USER, but I need to make a function that is sane --
+# i.e. if the argument is a path to a file, return the directory the file is in,
+# but if the directory is already a directory, just return the absolute path to it
+function __absdirname() {
+	if [ -d "$1" ] ; then
+		echo $(__abspath "$1")
+	else
+		echo $(__abspath $(dirname "$1"))
+	fi
+}
+
+function __abspath() {
+	if [ -d "$1" ] ; then 
+		local DIR=$1
+	else 
+		local DIR=$(dirname "$1")
+	fi
+	__vmpushd $DIR
+	echo $PWD
+	__vmpopd
+}
+
+function __mount_image () {
+	local PARTITION_NUM=1
+	
+	if [ -e "$1" ] ; then # a real filename given 
+		local FILENAME=$( basename "$1" )
+		local FILE_DIR=$( __absdirname "$1" )
+	else
+		local FILENAME="$IMAGE_FILE"
+		local FILE_DIR="$IMAGE_DIR"
+	fi
+
+	local PARTITION_TYPE="ext2"
+	echo "Mounting $PARTITION_TYPE partition: image=$FILE_DIR/$FILENAME to $MOUNT_POINT ... "
+
+	local OFFSET=`sfdisk -l -uS "$FILE_DIR/$FILENAME" 2> /dev/null | grep "$FILENAME$PARTITION_NUM" | awk '{print $2}'`
+	local SECTOR_OFFSET=$((OFFSET*512))
+	sudo mount -t $PARTITION_TYPE -o loop,offset=$SECTOR_OFFSET "$FILE_DIR/$FILENAME" "$MOUNT_POINT"
+}
+
+function __umount_image () {
+	echo -n "unmounting image from $MOUNT_POINT ... "
+	__vmpushd $HOME 
+	sudo umount "$MOUNT_POINT";
+	__vmpopd 
+	sync
+	echo "OK"
+}
+
 function __mount_proc_sys_dev () {
 	sudo mount -o bind /proc "$MOUNT_POINT/proc"
 	sudo mount -o bind /dev "$MOUNT_POINT/dev"
@@ -208,12 +270,12 @@ function __unmount_proc_sys_dev () {
 	popd > /dev/null
 }
 
-function vmchroot() {
-	__mount_image $IMAGE_PATH; 
-	__mount_proc_sys_dev;
-	sudo chroot $MOUNT_POINT
-	__unmount_proc_sys_dev;
-	vmumount;
-}
 
+# get rid of the annoying output of these commands
+function __vmpushd() {
+	pushd "$1" > /dev/null
+}
+function __vmpopd() {
+	popd > /dev/null
+}
 
