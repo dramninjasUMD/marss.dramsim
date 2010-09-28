164ee75bcbe5ecaa464ea17bd4a861552026f2b0
Paul Rosenfeld <dramninjas@gmail.com>
2010-09-28 01:02:23 -0400

    More tweaks to vmutils.sh

--- vmutils.sh
+++ vmutils.sh
@@ -108,9 +108,22 @@ function vmrun() {
 		__vmpopd
 		return
 	fi 
+	if [ ! -f "hda$1.qcow2" ] ; then 
+		echo "ERROR: hda$1.qcow2 not found; can't start VM"
+		return
+	fi
+	local hdb_string=""
+	if [ -f "hdb$1.qcow2" ] ; then 
+		hdb_string="-hdb hdb$1.qcow2"
+	fi 
+	local hdc_string=""
+	if [ -f "hdc$1.raw" ] ; then 
+		hdb_string="-hdc hdc$1.raw"
+	fi 
 
+		
 	echo "Launching simulation #$1: $SIM_DESCRIPTION"
-	local CMD_TO_RUN="SIM_DESC=\"$SIM_DESCRIPTION\" gdb -x gdbrun -args qemu/qemu-system-x86_64 -m 2GB -simconfig \"simconfig$1.cfg\" -hda \"hda$1.qcow2\" -hdb \"hdb$1.qcow2\" -hdc \"hdc$1.raw\" -curses"
+	local CMD_TO_RUN="SIM_DESC=\"$SIM_DESCRIPTION\" qemu/qemu-system-x86_64 -m 2GB -net nic,model=ne2k_pci -net user -simconfig \"simconfig$1.cfg\" -hda \"hda$1.qcow2\" $hdb_string $hdc_string -curses"
 	screen -d -m -S "sim$1" bash -c "$CMD_TO_RUN"
 	__vmpopd
 }
@@ -125,15 +138,23 @@ function vmclean() {
 	__vmpopd
 }
 
-# vmsetup x desc -- setup vm x with a sim description 
+# vmsetup x [description] [shared|private] [nohdb] -- setup vm x with a sim description
 # This command creates the necessary files, copies a simulate.sh file into the hdc disk image which can be auto launched if the VM is setup the right way.
 # This function does the heavy lifting of this script -- if it sounds interesting, send dramninjas (at) gmail (dot) com an email and I can explain how
 # the disk images are supposed to be set up for this to work right
 
 function vmsetup() {
 	if [ $# -lt 1 ] ; then 
-		echo "usage: $0 sim_number [sim_description]"
+		echo "usage: $0 sim_number [sim_description] [shared|private] [nohdb]"
 		return
+	fi 
+	local usehdb=1
+	local cache_config_string=""
+	if [ "$3" == "shared" ] ; then
+		cache_config_string="-cache-config-type shared_L2"	
+	fi 
+	if [ "$4" == "nohdb" ] ; then 
+		usehdb=0
 	fi 
 	__vmpushd "$MARSS_DIR" 
 
@@ -142,12 +163,12 @@ function vmsetup() {
 -stats run$1.stats
 -logfile run$1.log
 -corefreq 2000000000
+$cache_config_string 
 EOF
 
 local SIMULATE_FILE_SRC="simulate$1.sh"
 local SIMULATE_FILE_DEST="$MOUNT_POINT/simulate.sh"
 
-cp "$IMAGE_DIR/hdc.raw" "hdc$1.raw"
 
 if [ -e "$SIMULATE_FILE_SRC" ] ; then 
 
@@ -159,6 +180,7 @@ if [ -e "$SIMULATE_FILE_SRC" ] ; then
 			echo "#SIM_DESC=\"$2\"" >> $SIMULATE_FILE_SRC
 		fi 
 	fi 
+	cp "$IMAGE_DIR/hdc.raw" "hdc$1.raw"
 
 	__mount_image "hdc$1.raw" 
 	echo "Copying $SIMULATE_FILE_SRC to $SIMULATE_FILE_DEST"
@@ -168,17 +190,22 @@ else
 	echo "WARNING: using default simulate.sh"
 fi
 
+	local images_to_clone="hda hdb"
+	if [ $usehdb == 0 ] ; then 
+		images_to_clone="hda"
+	fi
 
-for img in "hda" "hdb"
+for img in $images_to_clone
 do
 	local OUTPUT_IMG_NAME="${img}$1.qcow2"
-	local BASE_IMG_NAME="$IMAGE_DIR/${img}0.qcow2"
-	if [ -e "$BASE_IMG_NAME" ] ; then 
-		echo "copying $BASE_IMG_NAME to $OUTPUT_IMG_NAME"
-		cp "$BASE_IMG_NAME" "$OUTPUT_IMG_NAME"
-	else
+#FIXME: need to do an lsof first to make sure the image isn't being used by a simulation before trying to do this
+#	local BASE_IMG_NAME="$IMAGE_DIR/${img}0.qcow2"
+#	if [ -e "$BASE_IMG_NAME" ] ; then 
+#		echo "copying $BASE_IMG_NAME to $OUTPUT_IMG_NAME"
+#		cp "$BASE_IMG_NAME" "$OUTPUT_IMG_NAME"
+#	else
 		vmclone "$IMAGE_DIR/$img.raw" "$OUTPUT_IMG_NAME"
-	fi 
+#	fi 
 done
 
 __vmpopd
